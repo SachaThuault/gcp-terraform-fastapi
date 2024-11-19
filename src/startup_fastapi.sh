e71cf622ca91ee7fe8589ad1619b3d4191d4128f
Sacha Thuault <sacha.thuault@saint-gobain.com>
2024-11-19 18:24:05 +0100

    [API Server] Change startup script

--- src/startup_fastapi.sh
+++ src/startup_fastapi.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# # Define the flag file path
-# FLAG_FILE="/var/log/fastapi-startup-executed"
+# Define the flag file path
+FLAG_FILE="/var/log/fastapi-startup-executed"
 
 # Get the project ID from gcloud
 PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_ID
 
-# # Check if the script has already been executed
-# if [ -f $FLAG_FILE ]; then
-#     gsutil cp gs://$PROJECT_ID-bucket-data/main.py .
-#     echo "Startup script has already been executed. Reloading and starting FastAPI service."
-#     sudo systemctl daemon-reload
-#     sudo systemctl start fastapi
-#     sudo systemctl enable fastapi
-#     exit 0
-# fi
+# Check if the script has already been executed
+if [ -f $FLAG_FILE ]; then
+    gsutil cp gs://$PROJECT_ID-bucket-data/main.py .
+    echo "Startup script has already been executed. Reloading and starting FastAPI service."
+    sudo systemctl daemon-reload
+    sudo systemctl start fastapi
+    sudo systemctl enable fastapi
+    exit 0
+fi
 
 # If the script hasn't run before, proceed with installation and setup
 echo "Executing startup script for the first time."
@@ -23,6 +23,11 @@ echo "Executing startup script for the first time."
 # Update and install necessary packages
 sudo apt update
 sudo apt install -y python3 python3-pip
+
+# Set up the FastAPI application directory
+APP_DIR="/home/$(whoami)/fastapi_app"
+mkdir -p "$APP_DIR"
+cd "$APP_DIR"
 
 # Write the FastAPI application to main.py
 gsutil cp gs://$PROJECT_ID-bucket-data/main.py .
@@ -33,10 +38,6 @@ gsutil cp gs://$PROJECT_ID-bucket-data/requirements.txt .
 # Install python libraries
 pip3 install -r requirements.txt
 
-# Set up the FastAPI application directory
-APP_DIR="/home/$(whoami)/fastapi_app"
-mkdir -p "$APP_DIR"
-cd "$APP_DIR"
 
 # Add ~/.local/bin to PATH in current session
 export PATH=$PATH:~/.local/bin
@@ -72,5 +73,5 @@ sudo systemctl start fastapi
 sudo systemctl enable fastapi
 
 # Create the flag file to indicate successful execution
-# touch $FLAG_FILE
+touch $FLAG_FILE
 
